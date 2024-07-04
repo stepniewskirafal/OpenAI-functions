afdf2faa3b6b2ff795f28ae4562918bebb9a56cd
Rafal Stepniewski <vultur12@gmail.com>
2024-07-04 09:10:59 +0200

    init env

---
+++ stockagent/src/main/java/pl/stepniewski/stockagent/OpenAiFunctionsApplication.java
@@ -0,0 +1,13 @@
+package pl.stepniewski.stockagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenAiFunctionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenAiFunctionsApplication.class, args);
+	}
+
+}
---
+++ stockagent/src/test/java/pl/stepniewski/stockagent/OpenAiFunctionsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.stepniewski.stockagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenAiFunctionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
